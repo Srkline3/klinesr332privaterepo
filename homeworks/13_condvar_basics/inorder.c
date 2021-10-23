2d75958cd9fc13cf2fe47905910eff1a6a801b22
Trey Kline <srkline3@gmail.com>
2021-10-23 18:04:01 +0000

    hw

--- homeworks/13_condvar_basics/inorder.c
+++ homeworks/13_condvar_basics/inorder.c
@@ -2,31 +2,47 @@
 #include <pthread.h>
 #include <unistd.h>
 
+int nextThread = 1;
+pthread_cond_t wait_cond;
+pthread_mutex_t mutex;
+
 void *thread(void *arg)
 {
         int *num = (int *)arg;
+      
         printf("%d wants to enter the critical section\n", *num);
 
-        printf("%d is finished with the critical section\n", *num);
+        pthread_mutex_lock(&mutex);
+        while (nextThread != *num)
+        {
+                pthread_cond_wait(&wait_cond, &mutex);
+        }
 
+
+        
+        nextThread++;
+        printf("%d is finished with the critical section\n", *num);
+        pthread_cond_signal(&wait_cond);
+        pthread_mutex_unlock(&mutex);
         return NULL;
 }
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
         pthread_t threads[4];
         int i;
         int nums[] = {2, 1, 4, 3};
 
-        for(i = 0; i < 4; ++i) {
+        for (i = 0; i < 4; ++i)
+        {
                 pthread_create(&threads[i], NULL, thread, &nums[i]);
 
-                if(i == 2)
+                if (i == 2)
                         sleep(3);
         }
 
-        for(i = 0; i < 4; ++i) {
+        for (i = 0; i < 4; ++i)
+        {
                 pthread_join(threads[i], NULL);
         }
 
--- homeworks/13_condvar_basics/max.c
+++ homeworks/13_condvar_basics/max.c
@@ -2,33 +2,51 @@
 #include <pthread.h>
 #include <unistd.h>
 
+pthread_cond_t wait_cond;
+pthread_mutex_t mutex;
+int in_there = 0;
+
 void *thread(void *arg)
 {
 	char *letter = (char *)arg;
 	printf("%c wants to enter the critical section\n", *letter);
 
+	pthread_mutex_lock(&mutex);
+	while(in_there >= 3)
+	{
+		pthread_cond_wait(&wait_cond, &mutex);
+	}
+
+	in_there++;
+
 	printf("%c is in the critical section\n", *letter);
 	sleep(1);
 	printf("%c has left the critical section\n", *letter);
+	
+	in_there--;
+
+	pthread_cond_signal(&wait_cond);
+    pthread_mutex_unlock(&mutex);
 
 	return NULL;
 }
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	pthread_t threads[8];
 	int i;
 	char *letters = "abcdefgh";
 
-	for(i = 0; i < 8; ++i) {
+	for (i = 0; i < 8; ++i)
+	{
 		pthread_create(&threads[i], NULL, thread, &letters[i]);
 
-		if(i == 4)
+		if (i == 4)
 			sleep(4);
 	}
 
-	for(i = 0; i < 8; i++) {
+	for (i = 0; i < 8; i++)
+	{
 		pthread_join(threads[i], NULL);
 	}
 
